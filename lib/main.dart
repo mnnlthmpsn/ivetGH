f746a68796e33286b8a55c57aa70941185291bff
Emmanuel Thompson <e.thompson@appsNmobile.local>
2022-03-18 13:21:32 +0000

    con_rem

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,3 @@
-import 'dart:async';
-import 'dart:io';
-
-import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -10,7 +6,7 @@ import 'package:vetgh/screens/categories.dart';
 import 'package:vetgh/screens/home.dart';
 import 'package:vetgh/screens/nominees.dart';
 import 'package:vetgh/screens/splashScreen.dart';
-import 'dart:developer' as developer;
+import 'package:vetgh/screens/vote.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,40 +17,12 @@ void main() {
   });
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
-
-  bool hasInternet = true;
-
-  @override
-  void initState() {
-    super.initState();
-    hasNetwork();
-  }
-
-  Future<bool> hasNetwork() async {
-    try {
-      final result = await InternetAddress.lookup('example.com');
-      setState(() {
-        hasInternet = result.isNotEmpty && result[0].rawAddress.isNotEmpty;
-      });
-      return result.isNotEmpty && result[0].rawAddress.isNotEmpty;
-    } on SocketException catch (_) {
-      setState(() => hasInternet = false);
-      return false;
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       title: 'VetGH',
       debugShowCheckedModeBanner: false,
@@ -68,7 +36,7 @@ class _MyAppState extends State<MyApp> {
       initialRoute: '/',
       routes: {
         '/': (context) => const SplashScreen(),
-        'home': (context) => hasInternet ? const Home() : Text('No Internet'),
+        'home': (context) => const Home(),
       },
     );
   }
